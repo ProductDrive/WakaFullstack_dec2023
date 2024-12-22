21ee91acfab5e75e2e016f3cd0d4d4f79a52af90
afeexclusive <afeexclusive@gmail.com>
2024-12-22 23:12:15 +0000

    every 5

--- node_be/services/backgroundjobs.js
+++ node_be/services/backgroundjobs.js
@@ -1,13 +1,28 @@
 import cron from 'node-cron';
 import {sendNotification} from './notification.js';
 
-cron.schedule('0 */10 * * *', async () => {  // Every 5 minute
-    console.log('Running the background job to send push notifications');
 
-    try {   
+cron.schedule('*/5 * * * *', () => {
+     (async () => {
+      console.log('Running the background job to send push notifications');
+  
+      try {
         console.log('Sending notifications...', new Date());
         await sendNotification();
-    } catch (error) {
+      } catch (error) {
         console.error('Error sending notifications:', error);
-    }
-});
+      }
+     })();
+  });
+  
+
+// cron.schedule('0 * * * *', async () => {  // Every 5 minute
+//     console.log('Running the background job to send push notifications');
+
+//     try {   
+//         console.log('Sending notifications...', new Date());
+//         await sendNotification();
+//     } catch (error) {
+//         console.error('Error sending notifications:', error);
+//     }
+// });
